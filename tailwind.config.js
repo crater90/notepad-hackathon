0f63d720e11e9f02742c34ccc35cda890697dae0
crater90 <kamranmohd89@gmail.com>
2022-09-25 16:44:31 +0530

    made the template and added styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./build/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
