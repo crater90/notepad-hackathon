dc8fc9d8d17dfa7b3050a6799c32240b4861677d
crater90 <kamranmohd89@gmail.com>
2022-09-26 15:58:17 +0530

    added dark mode, some functions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: ["./build/**/*.html"],
   theme: {
     extend: {
+      backgroundColor: (theme) => ({
+        ...theme("colors"),
+        background: "#202124",
+      }),
+      textColor: (theme) => theme("colors"),
+      textColor: {
+        offwhite: "#e1e1e2",
+      },
       fontFamily: {
         'rubik': ['Rubik', 'sans-serif']
       }
